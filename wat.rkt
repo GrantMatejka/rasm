c352d6dc42b54642900cbf6a912dd5c7214ab7a8
Grant Matejka <grantmatejka1@gmail.com>
2022-05-11 16:13:29 -0700

    Add some clarifying comments and decompose expr list when assigning value to keep stack valid

--- wat.rkt
+++ wat.rkt
@@ -133,18 +133,23 @@
      ; TODO: See if we can combine these
      (if (equal? fn '__app)
          (let ((first-arg ((pe-helper env) (first args)))
-               (rest-args (append-map (pe-helper env) (rest args))))
+               (rest-args (map (pe-helper env) (rest args))))
            `((\; Apply a first class function \;)
              (local.set $__tmp_res ,@first-arg)
              ,@(build-arr rest-args '$__app_param_arr)
              (i32.load (i32.add (i32.const 5) (local.get $__tmp_res)))
-           (call_indirect (type $__function_type) (i32.load (i32.add (i32.const 1) (local.get $__tmp_res))))))
+             (call_indirect (type $__function_type) (i32.load (i32.add (i32.const 1) (local.get $__tmp_res))))))
          (list `(call ,(wat-name (hash-ref prims fn))
                       ,@(append-map (pe-helper env) args))))]
     [(IndirectCall fn args)
-     (let ((p-fn ((pe-helper env) (hash-ref env fn (lambda () (get-lam-binding fn))))))
-       `(,@(build-arr (append-map (pe-helper env) args) '$__app_param_arr)
+     (let ((p-fn ((pe-helper env) (hash-ref env fn (lambda () (get-lam-binding fn)))))
+           (p-args (map (pe-helper env) args)))
+       `((\; Preparing to Call ,fn \;)
+         (\; Build function\'s parameter list \;)
+         ,@(build-arr p-args '$__app_param_arr)
+         (\; Retrieve Function's env \;)
          (i32.load (i32.add (i32.const 5) ,@p-fn))
+         (\; Calling ,fn \;)
          (call_indirect (type $__function_type)
                         ,(if (hash-has-key? func-table fn)
                              `(i32.const ,(hash-ref func-table fn))
@@ -177,35 +182,35 @@
                     (error 'top-id "Cannot find expected global id ~a" id))]
     ; If we get here, we want to retrieve the value of the specified id
     [(? symbol? s) (letrec ((id&env (if (hash-has-key? env s)
-                                                 (let ((env-id (hash-ref env s))
-                                                       (new-env (make-hash (hash->list (hash-remove (make-immutable-hash (hash->list env)) s)))))
-                                                   (list env-id new-env))
-                                                 (list s env)))
-                                   (func (findf (lambda ([f : Closure]) (equal? (first id&env) (Closure-name f))) funcs)))
+                                        (let ((env-id (hash-ref env s))
+                                              (new-env (make-hash (hash->list (hash-remove (make-immutable-hash (hash->list env)) s)))))
+                                          (list env-id new-env))
+                                        (list s env)))
+                            (func (findf (lambda ([f : Closure]) (equal? (first id&env) (Closure-name f))) funcs)))
                      (define id (first id&env))
                      (define env (second id&env))
                      (cond
-                        [(local? id) (list `(local.get ,(wat-name id)))]
+                       [(local? id) (list `(local.get ,(wat-name id)))]
                        ; Every param should be assigned to a local value by now, so we don't need to dereference the param arr all the time
                        [(param? id) (list `(local.get ,(wat-name id)))]
-                        ; The env arr is just a flat array of pointers
+                       ; The env arr is just a flat array of pointers
                        [(env? id) (list `(local.get ,(wat-name id)))]
-                        [(global? id) (list `(global.get ,(wat-name id)))]
-                        ; If we ever encounter a function in this instance, it's not being applied, so either being assigned or passed
-                        [func  (let ((env-exprs (append-map (pe-helper env) (map Id-sym (Closure-env-params func)))))
+                       [(global? id) (list `(global.get ,(wat-name id)))]
+                       ; If we ever encounter a function in this instance, it's not being applied, so either being assigned or passed
+                       [func  (let ((env-exprs (map (pe-helper env) (map Id-sym (Closure-env-params func)))))
                                 `((call $__allocate_func (i32.const ,(hash-ref func-table id))
                                         ,@(build-arr env-exprs '$__clo_env_arr_helper))))]
-                        [else (error 'unknown "ERROR: Unknown Id ~v ~a ~a ~a ~a" id globals params env-params locals)]))]
+                       [else (error 'unknown "ERROR: Unknown Id ~v ~a ~a ~a ~a" id globals params env-params locals)]))]
     [(Float n) (list `(call $__allocate_float (f64.const ,n)))]
     [(Int n) (list `(call $__allocate_int (i64.const ,n)))]
     [other (error 'unsupported (~a expr))]))
 
 
-(define (build-arr [exprs : (Listof Sexp)] [list-name : (U '$__app_param_arr '$__clo_env_arr_helper)]) : (Listof Sexp)
+(define (build-arr [exprs : (Listof (Listof Sexp))] [list-name : (U '$__app_param_arr '$__clo_env_arr_helper)]) : (Listof Sexp)
   `((local.set ,list-name (call $__alloc (i32.const ,(* 4 (length exprs)))))
     ,@(map
        (lambda ([src : Sexp] [r : Real])
-         `(i32.store (i32.add (i32.const ,(* (real->int r) 4)) (local.get ,list-name)) ,src))
+         `(i32.store (i32.add (i32.const ,(* (real->int r) 4)) (local.get ,list-name)) ,@src))
        exprs
        (range (length exprs)))
     (local.get ,list-name)))
