494669cbfceb278088cd321274eb27c2dadb74d3
Grant Matejka <grantmatejka1@gmail.com>
2022-05-25 18:38:50 -0700

    Add new validation program and update to use ports

--- compiler.rkt
+++ compiler.rkt
@@ -19,39 +19,52 @@
 
 (define input (open-input-file in))
 
-(when dev (when (not (directory-exists? "dev")) (make-directory "dev")))
-(when dev (when (not (directory-exists? "dev/expanded")) (make-directory "dev/expanded")))
-(when dev (when (not (directory-exists? "dev/ast")) (make-directory "dev/ast")))
-
 (unless (input-port? input)
   (raise-user-error "no input specified"))
 
 (define basename (first (string-split (last (string-split in "/")) ".")))
 
+(define output (open-output-file
+                (string-append "out/" basename ".wat")
+                #:exists 'replace))
+
 (define EXPANDED (expand-file in))
-(when dev (display-to-file EXPANDED
-                           (string-append "dev/expanded/" basename "_exp.rkt")
-                           #:exists 'replace))
 
 (define AST (build-ast EXPANDED))
-(when dev (pretty-display AST (open-output-file
-                               (string-append "dev/ast/" basename "_ast.rkt")
-                               #:exists 'replace)))
 
 (define PASSED-AST (full-pass AST))
-(when dev (pretty-display PASSED-AST (open-output-file
-                                      (string-append "dev/ast/" basename "_passed_ast.rkt")
-                                      #:exists 'replace)))
 
 (define WAT (build-wat PASSED-AST))
+
+; When we want to save dev records
+(when dev
+  (when (not (directory-exists? "dev")) (make-directory "dev"))
+  (when (not (directory-exists? "dev/expanded")) (make-directory "dev/expanded"))
+  (when (not (directory-exists? "dev/ast")) (make-directory "dev/ast"))
+  (define output-exp (open-output-file
+                      (string-append "dev/expanded/" basename "_exp.rkt")
+                      #:exists 'replace))
+  (define output-ast (open-output-file
+                      (string-append "dev/ast/" basename "_ast.rkt")
+                      #:exists 'replace))
+  (define output-past (open-output-file
+                       (string-append "dev/ast/" basename "_passed_ast.rkt")
+                       #:exists 'replace))
+  (pretty-display EXPANDED output-exp)
+  (pretty-display AST output-ast)
+  (pretty-display PASSED-AST output-past)
+  (close-output-port output-exp)
+  (close-output-port output-ast)
+  (close-output-port output-past))
+
 (when (not (directory-exists? "out")) (make-directory "out"))
 ; Copy over the rasm js module and the example index for easy operating
 (copy-file (string->path "./rasm.js") (string->path "./out/rasm.js") #t)
 (copy-file (string->path "./example_index.js") (string->path "./out/index.js") #t)
-(pretty-display WAT (open-output-file
-                     (string-append "out/" basename ".wat")
-                     #:exists 'replace))
+(pretty-display WAT output)
 
+(close-input-port input)
+(close-output-port output)
 
 
 
---
+++ examples/add1_C.rkt
@@ -0,0 +1,5 @@
+#lang racket
+(module add racket
+  (provide add1)
+  (define add1 (lambda (x) (+ x 1)))
+)
