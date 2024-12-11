2b922d6989e1b361f5e82162c05b915ede511f05
Hao Zhang <zhanghao@antigen.top>
2024-12-12 01:13:03 +0800

    Update info.rkt to enable command line use

--- info.rkt
+++ info.rkt
@@ -3,3 +3,6 @@
 (define collection "rasm")
 (define deps '("base" "typed-racket-lib"))
 (define license '(MIT))
+(define racket-launcher-libraries '("compiler.rkt"))
+(define racket-launcher-names '("rasm-compile"))
+(define raco-commands '(("rasm-compile" rasm/compiler "A Racket to WASM compiler." #f)))
